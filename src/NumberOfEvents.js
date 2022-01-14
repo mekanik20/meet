e041080b154f267df2978e3fad06dd836329567f
Mike H <72580577+mekanik20@users.noreply.github.com>
2022-01-13 21:18:33 -0500

    Updating NumberOfEvents

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 10
+  }
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+
+  }
+
+}
