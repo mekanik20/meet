25561261522d1689a05d9b3baeff0dcc67a0031c
Mike H <72580577+mekanik20@users.noreply.github.com>
2022-02-18 15:51:08 -0500

    Task 4.5 updated

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -36,3 +36,43 @@ describe('show/hide an event details', () => {
     expect(eventDetails).toBeNull();
   });
 });
+
+describe('Filter events by city.', () => {
+  let browser;
+  let page;
+  jest.setTimeout(50000);
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      /*headless: false,
+      slowMo: 250,
+      ignoreDefaultArgs: ['--disable-extensions'],*/
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('Events list of all cities will be rendered when on main page', async () => {
+    const countEvents = await page.$$eval('.event', (element) => element.length);
+    expect(countEvents).toBe(3);
+  });
+
+  test('Input text will render city suggestions', async () => {
+    await page.type('.city', 'Berlin');
+    const countCities = await page.$$eval('.suggestions li', (element) => element.length);
+    expect(countCities).toBe(2);
+  });
+
+  test('User clicks on suggested city from the list', async () => {
+    await page.reload();
+    await page.type('.city', 'Berlin');
+    await page.click('.suggestions li');
+    const countEvents = await page.$$eval('.event', (element) => element.length);
+    expect(countEvents).toBe(1);
+  });
+});
+
