05b3116d098321cd722740c3400828236e4deb4a
Mike H <72580577+mekanik20@users.noreply.github.com>
2022-03-03 15:12:46 -0500

    Fixing page load issue

--- src/api.js
+++ src/api.js
@@ -80,7 +80,7 @@ export const getEvents = async () => {
   }
 };
 
-export const getAccessToken = async () => {
+const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
   const tokenCheck = accessToken && (await checkToken(accessToken));
 
